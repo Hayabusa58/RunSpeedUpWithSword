77b876b00e73abb73ce5d478c11f952f57988c98
Hayabusa <yuuta.keiko.k@live.jp>
2016-07-16 14:19:39 +0900

    Register event.
    - イベントの登録

--- src/main/java/me/peregrine/RunSpeedUpWithSwordCore.java
+++ src/main/java/me/peregrine/RunSpeedUpWithSwordCore.java
@@ -1,15 +1,23 @@
 package main.java.me.peregrine;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-/* The main class this mod. 
- * @auther Hayabusa         
+/* The main class this mod.
+ * @auther Hayabusa
  */
 @Mod(name = "RunSpeedUpWithSword", modid = "me.peregrine.runspeedupwithsword", version = "MC1.7.10_1.0.0")
 public class RunSpeedUpWithSwordCore {
 	@Instance("me.peregrine.runspeedupwithsword")
 	public static RunSpeedUpWithSwordCore instance;
-	
-	
+
+	public static RunSpeedUpWithSwordEventHandler eh = new RunSpeedUpWithSwordEventHandler();
+
+	@EventHandler
+	public static void init(FMLInitializationEvent event){
+		FMLCommonHandler.instance().bus().register(eh);
+	}
 }
--- src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
+++ src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
@@ -1,5 +1,12 @@
 package main.java.me.peregrine;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+
 public class RunSpeedUpWithSwordEventHandler {
 
+	@SubscribeEvent
+	public void onPlayerTick(TickEvent.PlayerTickEvent event){
+		System.out.println("test");
+	}
 }
