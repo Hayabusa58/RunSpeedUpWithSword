ea4f41158033e76731a46466137c2cde7c4f51dc
Hayabusa <yuuta.keiko.k@live.jp>
2016-07-16 14:07:27 +0900

    Create main class.
    - メインクラスの作成，modid等の定義

---
+++ src/main/java/me/peregrine/RunSpeedUpWithSwordCore.java
@@ -0,0 +1,15 @@
+package main.java.me.peregrine;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+
+/* The main class this mod. 
+ * @auther Hayabusa         
+ */
+@Mod(name = "RunSpeedUpWithSword", modid = "me.peregrine.runspeedupwithsword", version = "MC1.7.10_1.0.0")
+public class RunSpeedUpWithSwordCore {
+	@Instance("me.peregrine.runspeedupwithsword")
+	public static RunSpeedUpWithSwordCore instance;
+	
+	
+}
