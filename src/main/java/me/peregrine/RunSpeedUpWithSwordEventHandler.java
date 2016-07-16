8c38b3e8b57d280235896053fd35bfdb938a1371
Hayabusa <yuuta.keiko.k@live.jp>
2016-07-16 18:00:19 +0900

    Change check method.
    - チェック用メソッドを剣の種類別に判定できるように変更

--- src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
+++ src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
@@ -14,24 +14,30 @@ public class RunSpeedUpWithSwordEventHandler {
 		EntityPlayer entP = event.player;
 		//EntityPlayerMP entMP = event.
 		ItemStack helditem = entP.getHeldItem();
-		if(!(entP == null) && this.isCurrentItemSwords(entP, helditem)){
+		if(!(entP == null) && !(this.checkKindsOfSwords(entP, helditem) == 0)){
 			/*Memo:初期値0.1F*/
 			//entP.capabilities.setPlayerWalkSpeed(10f);
 		}
 	}
 
-	boolean isCurrentItemSwords(EntityPlayer entP, ItemStack item){
+	/*
+	 * 剣以外を持っていた・何も持っていない場合0を返す
+	 * 木の剣を持っていた場合1，石の剣を持っていた場合2，金の剣を持っていた場合3，ダイアモンドの剣を持っていた場合4を返す */
+	int checkKindsOfSwords(EntityPlayer entP, ItemStack item){
 		if(!(entP == null) && !(item == null)){
 			if(!(item == entP.getHeldItem())){
-				return false;
+				return 0;
 			}
-			if (item.getItem() == Items.wooden_sword || item.getItem() == Items.stone_sword
-					|| item.getItem() ==  Items.golden_sword || item.getItem() == Items.diamond_sword) {
-				return true;
-			}else{
-				return false;
+			if (item.getItem() == Items.wooden_sword ) {
+				return 1;
+			}else if (item.getItem() == Items.stone_sword) {
+				return 2;
+			}else if (item.getItem() ==  Items.golden_sword) {
+				return 3;
+			}else if (item.getItem() == Items.diamond_sword) {
+				return 4;
 			}
 		}
-		return false;
+		return 0;
 	}
 }
