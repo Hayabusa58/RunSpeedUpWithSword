697a3e7e8e6f72491c80bc8daa133b6920a7bd99
Hayabusa <yuuta.keiko.k@live.jp>
2016-07-16 18:22:24 +0900

    Add changing walkspeed logic.
    - 持っている剣の種類によって移動速度が変化するように

--- src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
+++ src/main/java/me/peregrine/RunSpeedUpWithSwordEventHandler.java
@@ -14,15 +14,39 @@ public class RunSpeedUpWithSwordEventHandler {
 		EntityPlayer entP = event.player;
 		//EntityPlayerMP entMP = event.
 		ItemStack helditem = entP.getHeldItem();
+		
 		if(!(entP == null) && !(this.checkKindsOfSwords(entP, helditem) == 0)){
 			/*Memo:初期値0.1F*/
-			//entP.capabilities.setPlayerWalkSpeed(10f);
+			//必ず最初に初期化する
+			entP.capabilities.setPlayerWalkSpeed(0.1F);
+			float walkspeed = entP.capabilities.getWalkSpeed();
+			if(this.checkKindsOfSwords(entP, helditem) == 1){
+				//木の剣を持っていた場合，移動速度を1.5倍にする
+				entP.capabilities.setPlayerWalkSpeed(walkspeed * 1.5f);
+			}else if (this.checkKindsOfSwords(entP, helditem) == 2) {
+				//石の剣を持っていた場合，移動速度を2倍にする
+				entP.capabilities.setPlayerWalkSpeed(walkspeed * 2f);
+			}else if (this.checkKindsOfSwords(entP, helditem) == 3) {
+				//鉄の剣を持っていた場合，移動速度を2.5倍にする
+				entP.capabilities.setPlayerWalkSpeed(walkspeed * 2.5f); 
+			}else if (this.checkKindsOfSwords(entP, helditem) == 4) {
+				//金の剣を持っていた場合，移動速度を3倍にする
+				entP.capabilities.setPlayerWalkSpeed(walkspeed * 3f); 
+			}else if (this.checkKindsOfSwords(entP, helditem) == 5) {
+				//ダイヤモンドの剣を持っていた場合，移動速度を6倍にする（仮仕様）
+				entP.capabilities.setPlayerWalkSpeed(walkspeed *6f); 
+			}
+		}else{
+			entP.capabilities.setPlayerWalkSpeed(0.1F);
 		}
 	}
 
 	/*
 	 * 剣以外を持っていた・何も持っていない場合0を返す
-	 * 木の剣を持っていた場合1，石の剣を持っていた場合2，金の剣を持っていた場合3，ダイアモンドの剣を持っていた場合4を返す */
+	 * 木の剣を持っていた場合1，石の剣を持っていた場合2，鉄の剣を持っていた場合3，金の剣を持っていた場合4，ダイアモンドの剣を持っていた場合5を返す
+	 * 
+	 * 
+	 */
 	int checkKindsOfSwords(EntityPlayer entP, ItemStack item){
 		if(!(entP == null) && !(item == null)){
 			if(!(item == entP.getHeldItem())){
@@ -32,10 +56,12 @@ public class RunSpeedUpWithSwordEventHandler {
 				return 1;
 			}else if (item.getItem() == Items.stone_sword) {
 				return 2;
-			}else if (item.getItem() ==  Items.golden_sword) {
+			}else if (item.getItem() ==  Items.iron_sword) {
 				return 3;
-			}else if (item.getItem() == Items.diamond_sword) {
+			}else if (item.getItem() == Items.golden_sword) {
 				return 4;
+			}else if (item.getItem() == Items.diamond_sword) {
+				return 5;
 			}
 		}
 		return 0;
